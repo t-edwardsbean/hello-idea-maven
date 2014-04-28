2121d4463f3f2ded24ef106a79d076b0872fc5f5
edwardsbean <edwardsbean@gmail.com>
2014-04-28 13:19:57 +0800

    first commit

---
+++ hello-module-two/src/main/java/com/baidu/test/two/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.baidu.test.two;
+
+/**
+ * Created by Administrator on 2014/4/28.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
